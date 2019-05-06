c695483af5b44c09ad1e09ad50bd436fd81f87ed
HelloKitty91 <rajarajeswari6991@gmail.com>
2019-05-07 01:31:17 +0200

    Made production code more readable

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,7 +1,23 @@
 public class LeapYear {
 
     public boolean isLeapYear(int year){
-        return (year%400==0 || (year%4==0 && year%100!=0));
+        return ((isMultipleOf400(year) && !isMultipleOf4000(year)) || (isMultipleOf4(year) && !isMultipleOf100(year) && !isMultipleOf4000(year)));
+    }
+
+    private boolean isMultipleOf100(int year) {
+        return year%100==0;
+    }
+
+    private boolean isMultipleOf4(int year) {
+        return year%4==0;
+    }
+
+    private boolean isMultipleOf4000(int year) {
+        return year%4000==0;
+    }
+
+    private boolean isMultipleOf400(int year) {
+        return year%400==0;
     }
 
 }
