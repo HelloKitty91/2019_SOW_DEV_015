c13238176823080da79ae4c6382ce01c01c1372b
HelloKitty91 <rajarajeswari6991@gmail.com>
2019-05-07 00:08:52 +0200

    Avoid duplication by initializing LeapYear setup

--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,19 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LeapYearTest {
 
+    private LeapYear leapYear;
+
+    @Before
+    public void setUp() throws Exception{
+        leapYear = new LeapYear();
+    }
+
     @Test
     public void test_isLeapYear_whenDivisibleBy400_shouldReturnTrue(){
-        LeapYear leapYear = new LeapYear();
         Boolean isLeap = leapYear.isLeapYear(2000);
         assertTrue(isLeap);
     }
 
     @Test
     public void test_isLeapYear_whenDivisibleBy100ButNotBy400_shouldReturnFalse(){
-        LeapYear leapYear = new LeapYear();
         Boolean isLeap = leapYear.isLeapYear(2100);
         assertFalse(isLeap);
     }
