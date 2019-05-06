d031b3f3a920927ed780f6a73975be432c491f9e
HelloKitty91 <rajarajeswari6991@gmail.com>
2019-05-07 01:50:14 +0200

    Refactored by using object instead of primitive data type

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,23 +1,26 @@
+import java.time.Year;
+
 public class LeapYear {
 
-    public boolean isLeapYear(int year){
-        return ((isMultipleOf400(year) && !isMultipleOf4000(year)) || (isMultipleOf4(year) && !isMultipleOf100(year) && !isMultipleOf4000(year)));
-    }
-
-    private boolean isMultipleOf100(int year) {
-        return year%100==0;
+    public boolean isLeapYear(Year year){
+        int integerValueOfYear = Integer.parseInt(String.valueOf(year));
+        return ((isMultipleOf400(integerValueOfYear) && !isMultipleOf4000(integerValueOfYear)) || (isMultipleOf4(integerValueOfYear) && !isMultipleOf100(integerValueOfYear) && !isMultipleOf4000(integerValueOfYear)));
     }
 
     private boolean isMultipleOf4(int year) {
         return year%4==0;
     }
 
-    private boolean isMultipleOf4000(int year) {
-        return year%4000==0;
+    private boolean isMultipleOf100(int year) {
+        return year%100==0;
     }
 
     private boolean isMultipleOf400(int year) {
         return year%400==0;
+    }
+
+    private boolean isMultipleOf4000(int year) {
+        return year%4000==0;
     }
 
 }
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Year;
+
 import static org.junit.Assert.*;
 
 public class LeapYearTest {
@@ -12,33 +14,33 @@ public class LeapYearTest {
         leapYear = new LeapYear();
     }
 
-    private boolean isLeapYear(int year) {
+    private boolean isLeapYear(Year year) {
         return leapYear.isLeapYear(year);
     }
 
     @Test
     public void test_isLeapYear_whenDivisibleBy400_shouldReturnTrue(){
-        assertTrue(isLeapYear(2000));
+        assertTrue(isLeapYear(Year.of(2000)));
     }
 
     @Test
     public void test_isLeapYear_whenDivisibleBy100ButNotBy400_shouldReturnFalse(){
-        assertFalse(isLeapYear(2100));
+        assertFalse(isLeapYear(Year.of(2100)));
     }
 
     @Test
     public void test_isLeapYear_whenDivisibleBy4ButNotBy100_shouldReturnTrue(){
-        assertTrue(isLeapYear(2008));
+        assertTrue(isLeapYear(Year.of(2008)));
     }
 
     @Test
     public void test_isLeapYear_whenNotDivisibleBy4_shouldReturnFalse(){
-        assertFalse(isLeapYear(2019));
+        assertFalse(isLeapYear(Year.of(2019)));
     }
 
     @Test
     public void test_isLeapYear_whenDivisibleBy4000_shouldReturnFalse(){
-        assertFalse(isLeapYear(16000));
+        assertFalse(isLeapYear(Year.of(16000)));
     }
 
 }
